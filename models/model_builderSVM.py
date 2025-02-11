451d202e7c62d1b08eb33e4185abbf86676611b5
Hugo Aupérin <hugo.auperin@devinci.fr>
2025-02-11 14:00:33 +0100

    SVM

--- models/model_builderSVM.py
+++ models/model_builderSVM.py
@@ -1,6 +1,6 @@
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 import joblib
 
 iris = load_iris()
@@ -9,9 +9,9 @@ y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = RandomForestClassifier(n_estimators=100, random_state=42)
+model = SVC(kernel='rbf', probability=True)
 model.fit(X_train, y_train)
 
-joblib.dump(model, "iris_model.pkl")
+joblib.dump(model, "models/iris_modelSVM.pkl")
 
-print("Modèle entraîné et sauvegardé sous 'iris_model.pkl'")
+print("Modèle entraîné et sauvegardé sous 'iris_modelSVM.pkl'")
